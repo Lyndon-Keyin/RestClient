fc83197a04e4e6439e04826cf679b06942dd9bba
lloveys <lyndon.loveys@keyin.com>
2021-11-30 08:12:51 -0330

    Upto date Client

--- src/main/java/RestClient.java
+++ src/main/java/RestClient.java
@@ -1,5 +1,7 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.util.JSONPObject;
+import net.bytebuddy.implementation.bytecode.Throw;
+import org.apache.commons.logging.Log;
 
 import java.io.IOException;
 import java.net.URI;
@@ -16,113 +18,139 @@ import java.util.Scanner;
 public class RestClient {
 
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        //httpPOSTRequest();
-        //httpGetRequest();
-        httpPutRequest();
-        //httpDeleteRequest();
-    }
-
-    public static void httpGetRequest() throws IOException, InterruptedException {
-       Scanner scanner = new Scanner(System.in);
-        String search;
-        System.out.println("Please enter a number.");
-        search = scanner.next();
-
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://localhost:8080/members")).build();
-
-        try {
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            if (response.statusCode()==200) {
-                System.out.println("*****" + response.body());
+    public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            String request;
+            System.out.println("Input which request you wish to perform GET(search), POST(Input data), PUT(update data or DELETE data): ");
+            request = input.nextLine().toUpperCase();
+            System.out.println();
+            try {
+                if (request.equals("GET")) {
+                    httpGetRequest();//gets all in Database
+                } else if (request.equals("POST")) {
+                    httpPOSTRequest();
+                } else if (request.equals("PUT")) {
+                    httpPutRequest();
+                } else {
+                    httpDeleteRequest();
+                }
+            } catch (Exception e) {
+                new Exception("must be one of the given examples please try again");
+                e.printStackTrace();
             }
-
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
         }
-    }
 
-    public static void httpPOSTRequest() throws IOException, InterruptedException {
+        public static void httpGetRequest () throws IOException, InterruptedException {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://localhost:8080/member")).build();
+
+            try {
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                if (response.statusCode() == 200) {
+                    System.out.println("*****" + response.body());
+                }
+
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public static void httpPOSTRequest () throws IOException, InterruptedException {
             Map<Object, Object> data = new HashMap<>();
-            data.put("firstName", "Triple");
-            data.put("lastName", "D");
-            data.put("address", "33 that street");
-            data.put("email", "webfiu@gmail.com");
-            data.put("phone", 5555555);
-            data.put("durationOfMembership", 1);
-            data.put("membershipType", "VIP");
-            data.put("currentNumTournaments", 3);
-            data.put("pastNumTournaments", 2);
-            data.put("futureNumTournaments", 6);
+            data.put("firstName", "Tom");
+            data.put("lastName", "Hanks");
+            data.put("address", "2 that street");
+            data.put("email", "jdoiwhec@gmail.com");
+            data.put("phone", 9283094);
+            data.put("durationOfMembership", 2);
+            //          data.put("membershipType", 107);
+            data.put("currentTournaments", 122);
+            data.put("pastTournaments", 123);
+            data.put("futureTournaments", 124);
+            data.put("startOfMembership", "05/05/2019");
 
-            data.put("finalStandings", "36th");
-            data.put("location", "St. John's");
-            data.put("participationMembers", "Double D");
+//            data.put("finalStandings", "36th");
+//            data.put("location", "St. John's");
+//            data.put("participationMembers", "Double D");
             data.put("ts", System.currentTimeMillis());
 
             ObjectMapper dataMapper = new ObjectMapper();
             String stringifydataMapper = dataMapper.writeValueAsString(data);
-
-            HttpClient client = HttpClient.newBuilder()
-                    .version(HttpClient.Version.HTTP_2)
-                    .build();
-            HttpRequest request = HttpRequest.newBuilder(URI.create("http://localhost:8080/save/"))
-                    .version(HttpClient.Version.HTTP_2)
-                    .header("Content-Type", "application/json")
-                    .POST(HttpRequest.BodyPublishers.ofString(stringifydataMapper))
-                    .build();
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            String responseBody = response.body();
-            System.out.println("httpPostRequest : " + responseBody);
+            try {
+                HttpClient client = HttpClient.newBuilder()
+                        .version(HttpClient.Version.HTTP_2)
+                        .build();
+                HttpRequest request = HttpRequest.newBuilder(URI.create("http://localhost:8080/member"))
+                        .version(HttpClient.Version.HTTP_2)
+                        .header("Content-Type", "application/json")
+                        .POST(HttpRequest.BodyPublishers.ofString(stringifydataMapper))
+                        .build();
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                String responseBody = response.body();
+                System.out.println("httpPostRequest : " + responseBody);
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
         }
 
-    public static void httpPutRequest() throws IOException, InterruptedException{
-        Map<Object, Object> data = new HashMap<>();
-        data.put("firstName", "Triple");
-        data.put("lastName", "Doe");
-        data.put("address", "33 that street");
-        data.put("email", "webfiu@gmail.com");
-        data.put("phone", 5555555);
-        data.put("durationOfMembership", 1);
-        data.put("membershipType", "VIP");
-        data.put("currentNumTournaments", 3);
-        data.put("pastNumTournaments", 2);
-        data.put("futureNumTournaments", 6);
-        data.put("entryFee", 100);
-        data.put("cashPrize", 500);
-        data.put("finalStandings", "36th");
-        data.put("location", "St. John's");
-        data.put("participatingMembers", "Double D");
+        public static void httpPutRequest () throws IOException, InterruptedException {
+            Map<Object, Object> data = new HashMap<>();
+        data.put("firstName", "Tom");
+        data.put("lastName", "Hanks");
+        data.put("address", "2 that street");
+        data.put("email", "jdoiwhec@gmail.com");
+        data.put("phone", 9283094);
+        data.put("durationOfMembership", 2);
+        //          data.put("membershipType", 107);
+        data.put("currentTournaments", 122);
+        data.put("pastTournaments", 123);
+        data.put("futureTournaments", 124);
+        data.put("startOfMembership", "06/05/2019");
+       // data.put("pastNumTournaments", 123);
+      // data.put("past_tournaments", 123);
+//        data.put("entryFee", 100);
+//        data.put("cashPrize", 500);
+//        data.put("finalStandings", "36th");
+//        data.put("location", "St. John's");
+//        data.put("participatingMembers", "Double D");
 
-        data.put("ts", System.currentTimeMillis());
+            data.put("ts", System.currentTimeMillis());
 
-        ObjectMapper dataMapper = new ObjectMapper();
-        String stringifydataMapper = dataMapper.writeValueAsString(data);
+            ObjectMapper dataMapper = new ObjectMapper();
+            String stringifydataMapper = dataMapper.writeValueAsString(data);
+            try {
+                HttpRequest request = HttpRequest.newBuilder(URI.create("http://localhost:8080/updateMember/115"))
+                        .version(HttpClient.Version.HTTP_2)
+                        .header("Content-Type", "application/json")
+                        .PUT(HttpRequest.BodyPublishers.ofString(stringifydataMapper))
+                        .build();
 
-        HttpRequest request = HttpRequest.newBuilder(URI.create("http://localhost:8080/update/45"))
-                .version(HttpClient.Version.HTTP_2)
-                .header("Content-Type", "application/json")
-                .PUT(HttpRequest.BodyPublishers.ofString(stringifydataMapper))
-                .build();
+                HttpClient client = HttpClient.newHttpClient();
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                String responseBody = response.body();
+                System.out.println("httpPostRequest : " + responseBody);
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        String responseBody = response.body();
-        System.out.println("httpPostRequest : " + responseBody);
+        }
+
+        public static void httpDeleteRequest () throws IOException, InterruptedException {
+            try {
+                HttpRequest request = HttpRequest.newBuilder(URI.create("http://localhost:8080/deleteMember/136"))
+                        .version(HttpClient.Version.HTTP_2)
+                        .header("Content-Type", "application/json")
+                        .DELETE()
+                        .build();
+
+                HttpClient client = HttpClient.newHttpClient();
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                String responseBody = response.body();
+                System.out.println("httpPostRequest : " + responseBody);
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
-    public static void httpDeleteRequest() throws IOException, InterruptedException{
-        HttpRequest request = HttpRequest.newBuilder(URI.create("http://localhost:8080/delete/29"))
-                .version(HttpClient.Version.HTTP_2)
-                .header("Content-Type", "application/json")
-                .DELETE()
-                .build();
-
-        HttpClient client = HttpClient.newHttpClient();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        String responseBody = response.body();
-        System.out.println("httpPostRequest : " + responseBody);
-    }
-
-}
